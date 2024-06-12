545ece44e03fea36e5bf8cc2334f0cb3d6330d42
Keshawa_Udana <125798571+KeshUdana@users.noreply.github.com>
2024-06-12 11:15:30 +0000

    rrr

--- 12_6.py
+++ 12_6.py
@@ -9,6 +9,7 @@ a = int(input())
 b = int(input())
 print(a//b)
 print(a/b)
+
 """Task
 The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
 
@@ -21,3 +22,27 @@ while n>0 and n!=x:
     y=x*x
     x=x+1
     print(y)
+
+"""Given a year, determine whether it is a leap year. 
+If it is a leap year, return the Boolean True, otherwise return False.
+Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. 
+It is only necessary to complete the is_leap function."""
+def is_leap(year):
+    leap = False
+    if 1900 <= year <= 100000:
+        if (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
+            leap = True
+    return leap
+
+year = int(input())
+print(is_leap(year))
+
+"""The included code stub will read an integer, , from STDIN.
+Without using any string methods, try to print the following:
+Note that "" represents the consecutive values in between
+Print the list of integers from  through  as a string, without spaces."""
+n = int(input())
+x=1
+while x<=n and 1<=n<=150:
+    print(x,end='')
+    x=x+1
